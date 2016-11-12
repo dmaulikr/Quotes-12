0267aa75f92a6b910a6dce3a1a21267ae4094fcb
Marius Muntean <nima@mariuss-macbook-pro.local>
2016-11-12 17:23:17 +0100

    Initial Commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Marius Muntean on 12/11/16.
+//  Copyright © 2016 Marius Muntean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
